95a4b3a6d413417025d440be2cee77c7e8787b0a
17alma41 <17alma41@gmail.com>
2024-10-07 18:34:44 +0200

    assets in hardllvl

---
+++ Assets/Scripts/Map/PlatformMove.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformMove : MonoBehaviour
+{
+    [SerializeField] Transform objectToMove;
+    [SerializeField] Transform[] points;
+    [SerializeField] AnimationCurve ease;
+    [SerializeField] float animationDuration;
+
+    int from = 0;
+    int to = 1;
+    float elapsedTime;
+    bool canMove = false;  // Nueva variable para controlar si la plataforma puede moverse
+
+    void Start()
+    {
+        // Iniciamos la coroutine, pero la plataforma no se moverá hasta que se active el trigger
+        StartCoroutine(AnimationLinearInterpolation());
+    }
+
+    IEnumerator AnimationLinearInterpolation()
+    {
+        while (true)
+        {
+            
+            elapsedTime = 0;
+
+            while (elapsedTime < animationDuration)
+            {
+                elapsedTime += Time.deltaTime;
+
+                objectToMove.position = Vector3.LerpUnclamped(
+                    points[from].position,
+                    points[to].position,
+                    ease.Evaluate(elapsedTime / animationDuration)
+                );
+
+                yield return null;
+            }
+
+            IndexCount();
+            
+
+            yield return null;
+        }
+    }
+
+    void IndexCount()
+    {
+        from = to;
+        to = (to + 1) % points.Length;
+    }
+
+    /*
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            print("puedo entrar");
+            canMove = true;
+        }
+    }
+    */
+}
+
