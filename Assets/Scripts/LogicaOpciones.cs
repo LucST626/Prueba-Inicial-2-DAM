9d2ac32ac36b2d29ffc1487bf8a7fdc45ed12d7d
17alma41 <17alma41@gmail.com>
2024-10-09 00:07:02 +0200

    add: more assets and scripts

---
+++ Assets/Scripts/LogicaOpciones.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaOpciones : MonoBehaviour
+{
+    public ControladorOpciones panelOpciones;
+    private bool isPaused = false; // Estado de pausa
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelOpciones = GameObject.FindGameObjectWithTag("Opciones").GetComponent<ControladorOpciones>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame(); // Reanudar el juego si está pausado
+            }
+            else
+            {
+                MostrarOpciones(); // Mostrar las opciones y pausar el juego
+            }
+        }
+    }
+
+    public void MostrarOpciones()
+    {
+        panelOpciones.pantallaOpciones.SetActive(true); // Mostrar el panel de opciones
+        Time.timeScale = 0f; // Pausar el juego
+        isPaused = true; // Cambiar el estado a pausado
+    }
+
+    public void ResumeGame()
+    {
+        panelOpciones.pantallaOpciones.SetActive(false); // Ocultar el panel de opciones
+        Time.timeScale = 1f; // Reanudar el juego
+        isPaused = false; // Cambiar el estado a no pausado
+    }
+}
