70a467e536bba84a7db9b2353bf59df36567d799
17alma41 <17alma41@gmail.com>
2024-10-09 15:34:19 +0200

    add: cinematic and audio

---
+++ Assets/Scripts/Camera/Cinematic/CinematicScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class CinematicScene : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;  // El VideoPlayer asignado desde el Inspector
+    //public string gameplaySceneName; // Nombre de la escena de gameplay
+
+    void Start()
+    {
+        // Asegurarse de que el VideoPlayer está inicializado
+        videoPlayer.loopPointReached += OnVideoFinished; // Evento cuando el video termine
+    }
+
+    // Este método será llamado cuando termine el video
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        // Cargar la escena de gameplay
+        SceneManager.LoadScene("Tutorial");
+    }
+}
